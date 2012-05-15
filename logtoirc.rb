fa73be65468fc0620fbbec668e4edbf1cb9d8bc0
Jon Cowie <jcowie@etsy.com>
2012-05-15 09:43:09 +0100

    Adding extra comments and removing debug code

--- logtoirc.rb
+++ logtoirc.rb
@@ -1,3 +1,6 @@
+# This handler generates a gist of the last chef failure, and posts it to an IRC bot on a given TCP port.
+# It stores a copy of the last failure on disk, and only generates a new gist if the failure has changed.
+
 # Escape class can be found at https://github.com/akr/escape
 require 'escape'
 require 'socket'
@@ -13,6 +16,7 @@ module Etsy
       # Join the backtrace lines. Coerce to an array just in case.
       message << Array(backtrace).join("\n")
 
+      # Set path to file containing text dump of last fail
       lastfailfile = "#{Chef::Config[:file_cache_path]}/lastfail"
       
       ignore_hosts = [
@@ -23,31 +27,29 @@ module Etsy
         return
       end
 
+      # Load the text of the last fail from file if it exists
       lastfailmsg = []
       if File.exists? lastfailfile
           f = File.open(lastfailfile, "r") 
           f.each {|line| lastfailmsg.push line}
       end
-        
+      
+      # If the new failure is different from the text of the last fail we stored...  
       if !lastfailmsg.drop(1).join.eql? "#{message}\n"
         # gist.rb can be found at http://github.com/defunkt/gist
         gist_command = "gist.rb"
+        
+        # then generate a gist of it
         gist = %x[#{Escape.shell_command(["echo", "#{message}"])} | /var/chef/handlers/#{gist_command}]
         
+        # and write a new last fail file
         File.open(lastfailfile, "w") do |data|
           data.puts "#{gist}"
           data.puts "#{message}"
         end
       else
+        # Otherwise, use the gist from the last fail - no need to generate a new one
         gist = lastfailmsg.first
-      end
-
-      File.open("#{Chef::Config[:file_cache_path]}/lastfaildebug", "w") do |data|
-          data.puts "#{gist}"
-          data.puts "#{message.inspect}"
-          data.puts "\n\n\n"
-          data.puts "#{lastfailmsg.first}"
-          data.puts "#{lastfailmsg.drop(1).join.inspect}"
       end
         
       # Color code
